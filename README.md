fdd5e5c937eaa79ffabd27c14744885fd4ac2c7d
Artur Zacniewski <a.zacniewski@gmail.com>
2023-05-24 20:14:50 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# django-mentoring
+### django-mentoring
+
+> Pomocniczy projekt do analizy optymalizacji w Django (bazuje na [artykule](https://www.devs-mentoring.pl/optymalizacja-w-django/) ze strony devs-mentoring.pl).
---
+++ university/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Specializations(models.TextChoices):
+    PYTHON = "Python"
+    JAVA = "Java"
+    DATA = "Data"
+    Frontend = "Frontend"
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=70)
+    specialization = models.CharField(max_length=30, choices=Specializations.choices)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=70)
+    bio = models.TextField()
+    mentor = models.ForeignKey(to=Mentor, on_delete=models.SET_NULL, null=True)
